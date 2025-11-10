2d4265464faed4352d55c6ce27fb7df2a7d68674
aksworns22 <wkdeogks17@gmail.com>
2025-11-10 23:44:44 +0900

    test(map size): verify that map size value must be integer

--- src/test/kotlin/ReadingMapFeatureTest.kt
+++ src/test/kotlin/ReadingMapFeatureTest.kt
@@ -3,6 +3,8 @@ import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
+
 
 class ReadingMapFeatureTest {
     @Test
@@ -15,6 +17,14 @@ class ReadingMapFeatureTest {
     @CsvSource(value = ["0, 1", "1, 0", "-1, 1", "1, -1", "0, 0", "-1, -1"])
     fun `지도 크기가 양수가 아니라면 예외를 발생시킨다`(width: Int, height: Int) {
         assertThatThrownBy { MapSize(width, height) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining(MapSize.INVALID_VALUE_ERROR_MESSAGE);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["a", "1.5", "1.0", "-1", "0", " ", "", "@"])
+    fun `지도 크기로 입력받은 값이 올바르지 않다면 예외를 발생시킨다`(invalidWidth: String) {
+        assertThatThrownBy { MapSize.of(invalidWidth, "1") }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining(MapSize.INVALID_VALUE_ERROR_MESSAGE);
     }
