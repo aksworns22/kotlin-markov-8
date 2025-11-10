c4f2366a71c6f7cf4adacb5eee31958ea87757a8
aksworns22 <wkdeogks17@gmail.com>
2025-11-10 23:14:01 +0900

    test(map size): verify that map size is positive number

--- src/test/kotlin/ReadingMapFeatureTest.kt
+++ src/test/kotlin/ReadingMapFeatureTest.kt
@@ -1,10 +1,21 @@
 import org.assertj.core.api.Assertions.assertThatCode
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class ReadingMapFeatureTest {
     @Test
     fun `지도 파일을 읽어온다`() {
         assertThatCode { DataLoader.load(Data.MAP).available() }
             .doesNotThrowAnyException()
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["0, 1", "1, 0", "-1, 1", "1, -1", "0, 0", "-1, -1"])
+    fun `지도 크기가 양수가 아니라면 예외를 발생시킨다`(width: Int, height: Int) {
+        assertThatThrownBy { MapSize(width, height) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining(MapSize.INVALID_VALUE_ERROR_MESSAGE);
     }
 }
