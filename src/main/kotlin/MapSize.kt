ab56c8e6596889f66de182a5bd3ae29a6aa4b391
aksworns22 <wkdeogks17@gmail.com>
2025-11-10 23:45:29 +0900

    feat(map size): check that size is integer

--- src/main/kotlin/MapSize.kt
+++ src/main/kotlin/MapSize.kt
@@ -5,5 +5,11 @@ data class MapSize(val width: Int, val height: Int) {
 
     companion object {
         const val INVALID_VALUE_ERROR_MESSAGE = "올바르지 않은 지도 크기입니다"
+        fun of(rawWidth: String, rawHeight: String): MapSize {
+            require(rawWidth.isBlank() || rawHeight.isBlank()) { INVALID_VALUE_ERROR_MESSAGE }
+            val width = rawWidth.toIntOrNull() ?: throw IllegalArgumentException(INVALID_VALUE_ERROR_MESSAGE)
+            val height = rawHeight.toIntOrNull() ?: throw IllegalArgumentException(INVALID_VALUE_ERROR_MESSAGE)
+            return MapSize(width, height)
+        }
     }
 }
