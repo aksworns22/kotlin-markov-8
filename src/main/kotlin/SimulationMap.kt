f2c1d3236361d9ec23bf73ad0d48d15fc90f5efd
jangdaehan <wkdeogks17@gmail.com>
2025-11-12 12:22:43 +0900

    refactor(simulation map): remove duplicated logic

--- src/main/kotlin/SimulationMap.kt
+++ src/main/kotlin/SimulationMap.kt
@@ -1,32 +1,34 @@
 data class Position(val x: Int, val y: Int)
 
+enum class Location(val symbol: String) {
+    START("s"), DESTINATION("d")
+}
+
 data class SimulationMap(
     val size: MapSize,
     val start: Position,
-    val end: Position,
+    val destination: Position,
     val current: Position
 ) {
     companion object {
         const val INVALID_SIZE_ERROR_MESSAGE = "선언된 크기와 실제 지도의 크기다 일치하지 않습니다"
-        fun of(mapSize: MapSize, rawMapData: List<String>): SimulationMap {
-            require(rawMapData.size == mapSize.height) { INVALID_SIZE_ERROR_MESSAGE }
-            val mapData = rawMapData.map { it.split(" ") }
-            mapData.forEach {
+        fun of(mapSize: MapSize, rawMap: List<String>): SimulationMap {
+            require(rawMap.size == mapSize.height) { INVALID_SIZE_ERROR_MESSAGE }
+            val map = rawMap.map { it.split(" ") }
+            map.forEach {
                 require(it.size == mapSize.width) { INVALID_SIZE_ERROR_MESSAGE }
             }
-            var start: Position = Position(0, 0)
-            var end: Position = Position(0, 0)
+            val start = getPosition(mapSize, map, Location.START)
+            val destination = getPosition(mapSize, map, Location.DESTINATION)
+            return SimulationMap(size = mapSize, start = start, destination = destination, current = start)
+        }
+
+        private fun getPosition(mapSize: MapSize, map: List<List<String>>, location: Location): Position {
             for (y in 0 until mapSize.height) {
-                for (x in 0 until mapSize.width) {
-                    if (mapData[y][x] == "s") {
-                        start = Position(x, y)
-                    }
-                    if (mapData[y][x] == "d") {
-                        end = Position(x, y)
-                    }
-                }
+                val x = map[y].indexOfFirst { it == location.symbol }
+                if (x >= 0) return Position(x, y)
             }
-            return SimulationMap(size = mapSize, start = start, end = end, current = start)
+            throw IllegalArgumentException("위치(${location.symbol})를 찾을 수 없습니다")
         }
     }
 }
