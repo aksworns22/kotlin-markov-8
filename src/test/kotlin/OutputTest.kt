3d347a1bd2af6569932b7c232023c95c5712f7f8
aksworns22 <wkdeogks17@gmail.com>
2025-11-13 21:11:08 +0900

    test(load map): verify output message

---
+++ src/test/kotlin/OutputTest.kt
@@ -0,0 +1,34 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+import java.io.OutputStream
+import java.io.PrintStream
+
+
+class OutputTest {
+    private lateinit var outputStream: OutputStream
+
+    @BeforeEach
+    fun setUp() {
+        outputStream = ByteArrayOutputStream()
+        System.setOut(PrintStream(outputStream))
+    }
+
+    @AfterEach
+    fun tearDown() {
+        System.setOut(System.out)
+        println(output())
+    }
+
+    fun output(): String {
+        return outputStream.toString().trim()
+    }
+
+    @Test
+    fun `지도를 성공적으로 읽어 처리한다면 성공 메시지를 출력한다`() {
+        SimulationMap.of(MapSize(2, 2), listOf("s .", ". d"), ConsoleOutput)
+        assertThat(output()).contains("[SUCCESS]")
+    }
+}
