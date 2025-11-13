f7c5440cf313765d4556f0e2fca75f65605c3a11
aksworns22 <wkdeogks17@gmail.com>
2025-11-13 12:52:22 +0900

    feat(simulation map): add waypoint to location

--- src/main/kotlin/SimulationMap.kt
+++ src/main/kotlin/SimulationMap.kt
@@ -1,7 +1,13 @@
 data class Position(val x: Int, val y: Int)
 
 enum class Location(val symbol: String) {
-    START("s"), DESTINATION("d")
+    START("s"), DESTINATION("d"), WAYPOINT(".");
+
+    companion object {
+        fun isValidSymbol(locationSymbol: String): Boolean {
+            return Location.entries.any { it.symbol == locationSymbol }
+        }
+    }
 }
 
 data class SimulationMap(
@@ -14,6 +20,7 @@ data class SimulationMap(
         const val INVALID_SIZE_ERROR_MESSAGE = "선언된 크기와 실제 지도의 크기다 일치하지 않습니다"
         const val LOCATION_FINDING_ERROR_MESSAGE = "위치를 찾을 수 없습니다"
         const val TOO_MANY_LOCATION_ERROR = "출발지 혹은 도착지는 여러개 존재할 수 없습니다"
+        const val INVALID_LOCATION_ERROR_MESSAGE = "알 수 없는 위치를 포함하고 있습니다"
         fun of(mapSize: MapSize, rawMap: List<String>): SimulationMap {
             require(rawMap.size == mapSize.height) { INVALID_SIZE_ERROR_MESSAGE }
             val map = rawMap.map { it.split(" ") }
@@ -23,6 +30,7 @@ data class SimulationMap(
             val start = getPosition(mapSize, map, Location.START)
             val destination = getPosition(mapSize, map, Location.DESTINATION)
             requireSingleStartAndDestination(map)
+            requireOnlyValidLocation(map)
             return SimulationMap(size = mapSize, start = start, destination = destination, current = start)
         }
 
@@ -42,6 +50,14 @@ data class SimulationMap(
         private fun countPosition(map: List<List<String>>, location: Location): Int {
             return map.sumOf { row ->
                 row.count { it == location.symbol }
+            }
+        }
+
+        private fun requireOnlyValidLocation(map: List<List<String>>) {
+            map.forEach { row ->
+                row.forEach {
+                    require(Location.isValidSymbol(it)) { INVALID_LOCATION_ERROR_MESSAGE }
+                }
             }
         }
 
