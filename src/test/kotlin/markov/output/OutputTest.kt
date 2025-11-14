de28f71dee9876ee9a2b7f6564082b92ba996a92
aksworns22 <wkdeogks17@gmail.com>
2025-11-15 01:34:51 +0900

    test(simulation): verify showing success simulation

--- src/test/kotlin/markov/output/OutputTest.kt
+++ src/test/kotlin/markov/output/OutputTest.kt
@@ -1,11 +1,22 @@
 package markov.output
 
 import markov.map.MapSize
+import markov.map.Position
 import markov.map.SimulationMap
+import markov.random.OnlyOneGenerator
+import markov.simulation.Action
+import markov.simulation.ActionType
+import markov.simulation.Moving
+import markov.simulation.Probability
+import markov.simulation.Simulation
+import markov.simulation.SimulationTime
+
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import java.io.ByteArrayOutputStream
 import java.io.OutputStream
 import java.io.PrintStream
@@ -31,7 +42,51 @@ class OutputTest {
 
     @Test
     fun `지도를 성공적으로 읽어 처리한다면 성공 메시지를 출력한다`() {
-        SimulationMap.Companion.of(MapSize(2, 2), listOf("s .", ". d"), ConsoleOutput)
+        SimulationMap.of(MapSize(2, 2), listOf("s .", ". d"), ConsoleOutput)
         Assertions.assertThat(output()).contains("[SUCCESS]")
+    }
+
+    @Test
+    fun `최종 목적지에 시간 내에 도달하는 경우 진행 상황을 콘솔에 출력한다`() {
+        val map = SimulationMap.of(MapSize(3, 1), listOf("s . d"), ConsoleOutput)
+        val limitTime = SimulationTime(5)
+        val moving = Moving(
+            mapOf(
+                Position(0, 0) to Action(
+                    mapOf(
+                        ActionType.UP to Probability.NO,
+                        ActionType.DOWN to Probability.NO,
+                        ActionType.LEFT to Probability.NO,
+                        ActionType.RIGHT to Probability(1, 100),
+                    )
+                ),
+                Position(1, 0) to Action(
+                    mapOf(
+                        ActionType.UP to Probability.NO,
+                        ActionType.DOWN to Probability.NO,
+                        ActionType.LEFT to Probability.NO,
+                        ActionType.RIGHT to Probability(1, 100),
+                    )
+                ),
+                Position(2, 0) to Action(
+                    mapOf(
+                        ActionType.UP to Probability.NO,
+                        ActionType.DOWN to Probability.NO,
+                        ActionType.LEFT to Probability.NO,
+                        ActionType.RIGHT to Probability(1, 100),
+                    )
+                )
+            )
+        )
+        Simulation.startFrom(map, limitTime, moving, OnlyOneGenerator, ConsoleOutput)
+        assertThat(output())
+            .contains(
+                listOf(
+                    "[시간: 0] x: 0, y: 0",
+                    "[시간: 1] x: 1, y: 0",
+                    "[시간: 2] x: 2, y: 0",
+                    "[최종 결과] 목적지에 시간 내에 도착했습니다"
+                )
+            )
     }
 }
