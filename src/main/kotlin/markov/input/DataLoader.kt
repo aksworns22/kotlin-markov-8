4248b24be6eafe80473cd0b8873a098635c855c1
aksworns22 <wkdeogks17@gmail.com>
2025-11-14 18:24:12 +0900

    fix(data loader): change load functionality

--- src/main/kotlin/markov/input/DataLoader.kt
+++ src/main/kotlin/markov/input/DataLoader.kt
@@ -6,7 +6,8 @@ import java.nio.file.Path
 
 object DataLoader {
     fun load(data: Data): InputStream {
-        return javaClass.getResourceAsStream(data.path) ?: throw IllegalStateException("File not found")
+        return this::class.java.classLoader.getResourceAsStream(data.path)
+            ?: throw IllegalStateException("File not found")
     }
 
     fun load(path: Path): InputStream {
