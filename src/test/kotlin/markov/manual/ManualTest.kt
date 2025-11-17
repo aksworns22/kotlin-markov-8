b7775c8e1a04c080eea61cb13da746b1c3e32824
aksworns22 <wkdeogks17@gmail.com>
2025-11-17 20:40:35 +0900

    test(manual): verify calculating distance in all positions

--- src/test/kotlin/markov/manual/ManualTest.kt
+++ src/test/kotlin/markov/manual/ManualTest.kt
@@ -1,7 +1,11 @@
 package markov.manual
 
+import markov.map.MapSize
 import markov.map.Position
+import markov.map.SimulationMap
+import markov.output.ConsoleOutput
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -12,6 +16,26 @@ class ManualTest {
     fun `도착지까지의 거리를 계산한다`(current: Position, destination: Position, expectedValue: Int) {
         assertThat(Distance.fromManhattan(current, destination))
             .isEqualTo(Distance(expectedValue))
+    }
+
+    @Test
+    fun `모든 위치에 대해 거리를 계산한다`() {
+        assertThat(DistanceMap.from(SimulationMap.of(MapSize(3, 3), listOf("s . .", ". d .", ". . ."), ConsoleOutput)))
+            .isEqualTo(
+                DistanceMap(
+                    mapOf(
+                        Position(0, 0) to Distance(2),
+                        Position(0, 1) to Distance(1),
+                        Position(0, 2) to Distance(2),
+                        Position(1, 0) to Distance(1),
+                        Position(1, 1) to Distance(0),
+                        Position(1, 2) to Distance(1),
+                        Position(2, 0) to Distance(2),
+                        Position(2, 1) to Distance(1),
+                        Position(2, 2) to Distance(2)
+                    )
+                )
+            )
     }
 
     companion object {
