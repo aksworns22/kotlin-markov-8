b2fbd03bcfb1586af7ed2758c243f5ba6f985ec8
aksworns22 <wkdeogks17@gmail.com>
2025-11-20 22:26:38 +0900

    feat(movement): verify compatibility with map size

--- src/main/kotlin/markov/movement/Movement.kt
+++ src/main/kotlin/markov/movement/Movement.kt
@@ -1,5 +1,6 @@
 package markov.movement
 
+import markov.map.MapSize
 import markov.map.Position
 
 @JvmInline
@@ -10,7 +11,14 @@ value class Movement(val probabilities: Map<Position, Action>) {
         return currentPosition.next(actions.chooseAction(probability))
     }
 
+    fun validateWith(mapSize: MapSize) {
+        val width = probabilities.keys.maxBy { it.x }.x - probabilities.keys.minBy { it.x }.x + 1
+        val height = probabilities.keys.maxBy { it.y }.y - probabilities.keys.minBy { it.y }.y + 1
+        require(width == mapSize.width && height == mapSize.height) { MAP_SIZE_COMPATIBLE_ERROR_MESSAGE }
+    }
+
     companion object {
+        const val MAP_SIZE_COMPATIBLE_ERROR_MESSAGE = "지도 크기와 맞지 않은 확률 정보입니다"
         fun of(rawMovements: List<String>): Movement {
             val probabilities = mutableMapOf<Position, Action>()
             for (movement in rawMovements) {
