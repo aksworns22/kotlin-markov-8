3a63edcff6a3e5e97bb46ba4d22ee9c31848296e
aksworns22 <wkdeogks17@gmail.com>
2025-11-21 00:04:28 +0900

    test(action): verify non negative probability input

--- src/test/kotlin/markov/movement/ActionTest.kt
+++ src/test/kotlin/markov/movement/ActionTest.kt
@@ -1,6 +1,7 @@
 package markov.movement
 
 import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -11,5 +12,12 @@ class ActionTest {
         assertThatThrownBy { Action.of(rawAction) }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage(Action.INVALID_PROBABILITY_ERROR_MESSAGE)
+    }
+
+    @Test
+    fun `이동 확률이 유효한 범위가 아니라면 예외를 발생시킨다`() {
+        assertThatThrownBy { Action.of("10,20,-10,80") }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(Action.NEGATIVE_PROBABILITY_ERROR_MESSAGE)
     }
 }
