bf08771ecf346e7fffbc76e21aaf450a137c5a50
aksworns22 <wkdeogks17@gmail.com>
2025-11-21 19:58:22 +0900

    feat(application): execute simulation and output results from file inputs

---
+++ src/main/kotlin/markov/Application.kt
@@ -0,0 +1,36 @@
+package markov
+
+import markov.input.Data
+import markov.input.DataLoader
+import markov.manual.ManualController
+import markov.map.MapReader
+import markov.map.SimulationMapController
+import markov.movement.MovementController
+import markov.movement.MovementReader
+import markov.output.Console
+import markov.random.RandomGenerator
+import markov.simulation.SimulationController
+import markov.simulation.SimulationTime
+import java.io.InputStream
+import kotlin.random.Random
+
+object OneToHundredGenerator : RandomGenerator {
+    override fun generate(): Int {
+        return Random.nextInt(1, 101)
+    }
+}
+
+fun runApplication(mapStream: InputStream, movementStream: InputStream) {
+    val map = SimulationMapController(Console).readMap(MapReader.read(mapStream))
+    val movement =
+        MovementController(
+            map!!.size,
+            Console
+        ).readMovement(MovementReader.read(movementStream))
+    ManualController(Console).findBestManual(map, movement!!)
+    SimulationController(Console).startFrom(map, SimulationTime(10), movement, OneToHundredGenerator)
+}
+
+fun main() {
+    runApplication(DataLoader.load(Data.MAP), DataLoader.load(Data.PROBABILITY))
+}
