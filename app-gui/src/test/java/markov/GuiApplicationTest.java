e6f0dd1499ed08e201b8ce43c789010bc8ec5f85
aksworns22 <wkdeogks17@gmail.com>
2025-11-22 01:00:18 +0900

    test(messageLogger): verify print loading map success message

--- app-gui/src/test/java/markov/GuiApplicationTest.java
+++ app-gui/src/test/java/markov/GuiApplicationTest.java
@@ -6,6 +6,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class GuiApplicationTest {
     private FrameFixture window;
 
@@ -24,5 +26,11 @@ public class GuiApplicationTest {
     @Test
     public void shouldPrintRequiredTitle() {
         window.requireTitle("Open Mission: Markov Reward Process");
+    }
+
+    @Test
+    public void shouldDisplayMessageWhenMapLoadsSuccessfully() {
+        String text = window.textBox("messageLog").text();
+        assertThat(text).contains("[SUCCESS]");
     }
 }
