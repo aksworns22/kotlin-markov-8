54693d2a7b914c64d713c5488de6088780dddb03
aksworns22 <wkdeogks17@gmail.com>
2025-11-21 13:48:44 +0900

    feat(movement controller): print error message

--- src/main/kotlin/markov/movement/MovementController.kt
+++ src/main/kotlin/markov/movement/MovementController.kt
@@ -6,10 +6,15 @@ import markov.output.MessageOutput
 import markov.output.MessageType
 
 class MovementController(val mapSize: MapSize, val output: MessageOutput) {
-    fun readMovement(rawMovement: List<String>): Movement {
-        val movement = Movement.of(rawMovement)
-        movement.validateWith(mapSize)
-        output.println(Message(MessageType.SUCCESS, "위치 별 이동 확률을 불러왔습니다"))
-        return movement
+    fun readMovement(rawMovement: List<String>): Movement? {
+        try {
+            val movement = Movement.of(rawMovement)
+            movement.validateWith(mapSize)
+            output.println(Message(MessageType.SUCCESS, "위치 별 이동 확률을 불러왔습니다"))
+            return movement
+        } catch (err: IllegalArgumentException) {
+            output.println(Message(MessageType.ERROR, err.message!!))
+        }
+        return null
     }
 }
