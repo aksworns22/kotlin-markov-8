8be81959f5549d6cbd86f172bfe531e0724dfa1b
aksworns22 <wkdeogks17@gmail.com>
2025-11-22 00:15:19 +0900

    test(main frame): verify window title

---
+++ app-gui/src/test/java/markov/GuiApplicationTest.java
@@ -0,0 +1,28 @@
+package markov;
+
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GuiApplicationTest {
+    private FrameFixture window;
+
+    @Before
+    public void onSetUp() {
+        MainFrame frame = GuiActionRunner.execute(() -> new MainFrame());
+        window = new FrameFixture(frame);
+        window.show();
+    }
+
+    @After
+    public void tearDown() {
+        window.cleanUp();
+    }
+
+    @Test
+    public void shouldPrintRequiredTitle() {
+        window.requireTitle("Open Mission: Markov Reward Process");
+    }
+}
