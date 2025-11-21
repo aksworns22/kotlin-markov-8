0bd132ba97f39d1721029635b53be7f028845224
aksworns22 <wkdeogks17@gmail.com>
2025-11-22 01:00:31 +0900

    feat(messageLogger): print loading map success message

--- app-gui/src/main/java/markov/MainFrame.java
+++ app-gui/src/main/java/markov/MainFrame.java
@@ -1,11 +1,19 @@
 package markov;
 
+import markov.input.Data;
+import markov.input.DataLoader;
+import markov.map.MapReader;
+import markov.map.SimulationMapController;
+
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
     MainFrame() {
         setTitle("Open Mission: Markov Reward Process");
         setSize(600, 600);
+        MessageLogger messageLog = new MessageLogger(10, 10);
+        add(messageLog);
         setVisible(true);
+        new SimulationMapController(messageLog).readMap(MapReader.INSTANCE.read(DataLoader.INSTANCE.load(Data.MAP)));
     }
 }
---
+++ app-gui/src/main/java/markov/MessageLogger.java
@@ -0,0 +1,19 @@
+package markov;
+
+import markov.output.Message;
+import markov.output.MessageOutput;
+
+import javax.swing.*;
+
+public class MessageLogger extends JTextArea implements MessageOutput {
+    MessageLogger(int rows, int cols) {
+        super(rows, cols);
+        setName("messageLog");
+        setEditable(false);
+    }
+
+    @Override
+    public void println(Message message) {
+        append(message.getFull());
+    }
+}
