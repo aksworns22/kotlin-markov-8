866fa49eebf700866436f61fcc62ae2228ccd79e
aksworns22 <wkdeogks17@gmail.com>
2025-11-22 00:15:43 +0900

    feat(main frame): print proper window title

---
+++ app-gui/src/main/java/markov/MainFrame.java
@@ -0,0 +1,11 @@
+package markov;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    MainFrame() {
+        setTitle("Open Mission: Markov Reward Process");
+        setSize(600, 600);
+        setVisible(true);
+    }
+}
