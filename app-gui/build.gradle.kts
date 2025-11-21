180377edfbbfb6686b74da5cd6e47ef22e59480c
aksworns22 <wkdeogks17@gmail.com>
2025-11-22 00:14:36 +0900

    chore(build.gradle.kts): configure app-gui build settings

---
+++ app-gui/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "com.github.aksworns22"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.vintage:junit-vintage-engine:6.1.0-M1")
+    implementation("org.assertj:assertj-swing:3.17.1")
+    implementation(project(":core"))
+}
