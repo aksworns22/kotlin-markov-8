e353fcbb714266bd8a955c328903f9fc69d7180c
aksworns22 <wkdeogks17@gmail.com>
2025-11-22 13:44:39 +0900

    feat(main frame): arrange log manager layout

--- app-gui/src/main/java/markov/MainFrame.java
+++ app-gui/src/main/java/markov/MainFrame.java
@@ -3,14 +3,22 @@ package markov;
 import markov.map.SimulationMapController;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.List;
 
 public class MainFrame extends JFrame {
     MainFrame(List<String> rawMap) {
         setTitle("Open Mission: Markov Reward Process");
         setSize(600, 600);
-        MessageLogger messageLog = new MessageLogger(10, 10);
-        add(messageLog);
+        setLayout(new BorderLayout());
+        MessageLogger messageLog = new MessageLogger(1, 1);
+        JScrollPane scrollPane = new JScrollPane(
+                messageLog,
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER
+        );
+        scrollPane.setPreferredSize(new Dimension(200, 100));
+        add(scrollPane, BorderLayout.SOUTH);
         setVisible(true);
         new SimulationMapController(messageLog).readMap(rawMap);
     }
