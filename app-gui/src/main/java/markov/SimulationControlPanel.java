2367f652300023e008180dcfb861ebfff92733cd
aksworns22 <wkdeogks17@gmail.com>
2025-11-23 14:44:21 +0900

    feat(simulationTime): display current simulation time

--- app-gui/src/main/java/markov/SimulationControlPanel.java
+++ app-gui/src/main/java/markov/SimulationControlPanel.java
@@ -3,11 +3,20 @@ package markov;
 import javax.swing.*;
 
 public class SimulationControlPanel extends JPanel {
+    private JLabel turnLabel;
+    private int currentTurn = 0;
 
     public SimulationControlPanel(SimulationPanel simulationPanel) {
         JButton nextButton = new JButton("Next Simulation");
         nextButton.setName("nextSimulation");
-        nextButton.addActionListener(e -> simulationPanel.paintSimulation());
+        nextButton.addActionListener(e -> {
+            simulationPanel.paintSimulation();
+            currentTurn += 1;
+            turnLabel.setText("현재 턴: " + currentTurn);
+        });
         add(nextButton);
+        this.turnLabel = new JLabel("현재 턴: 0");
+        this.turnLabel.setName("turn");
+        add(turnLabel);
     }
 }
