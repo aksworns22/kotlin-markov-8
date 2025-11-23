018a35245c3c18ab40e9212f4170b1e65d488b42
aksworns22 <wkdeogks17@gmail.com>
2025-11-23 16:59:57 +0900

    feat(application): update application to preview the actual app

--- app-gui/src/main/java/markov/Application.java
+++ app-gui/src/main/java/markov/Application.java
@@ -20,12 +20,20 @@ import java.awt.*;
 public class Application {
     public static void main(String[] args) {
         MessageLogger messageLogger = new MessageLogger();
-        SimulationPanel simulationPanel = new SimulationPanel();
+        SimulationMap map = new SimulationMapController(messageLogger).readMap(
+                MapReader.INSTANCE.read(DataLoader.INSTANCE.load(Data.MAP))
+        );
+        Movement movement = new MovementController(
+                map.getSize(),
+                messageLogger
+        ).readMovement(MovementReader.INSTANCE.read(DataLoader.INSTANCE.load(Data.PROBABILITY)));
+        SimulationPanel simulationPanel = new SimulationPanel(movement);
         MainFrame mainFrame = new MainFrame(messageLogger, simulationPanel);
-        SimulationMap map = new SimulationMapController(messageLogger).readMap(MapReader.INSTANCE.read(DataLoader.INSTANCE.load(Data.MAP)));
-        Movement movement = new MovementController(map.getSize(), messageLogger).readMovement(MovementReader.INSTANCE.read(DataLoader.INSTANCE.load(Data.PROBABILITY)));
         Manual manual = new ManualController(simulationPanel).findBestManual(map, movement);
-        new SimulationController(simulationPanel).startFrom(map, new SimulationTime(100), movement, OneToHundredGenerator.INSTANCE);
+        new SimulationController(simulationPanel).startFrom(
+                map,
+                new SimulationTime(10), movement, OneToHundredGenerator.INSTANCE
+        );
         simulationPanel.paintSimulation();
     }
 }
