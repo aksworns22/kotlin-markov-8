420a0c4c7f586c8196a08c5868eb836cab015dd3
aksworns22 <wkdeogks17@gmail.com>
2025-11-24 00:52:57 +0900

    test(simulation time): verify simulation time creation

--- core/src/test/kotlin/markov/input/SimulationInputTest.kt
+++ core/src/test/kotlin/markov/input/SimulationInputTest.kt
@@ -2,12 +2,17 @@ package markov.input
 
 import markov.simulation.SimulationTime
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class SimulationInputTest {
-    @Test
-    fun `시간 제한을 콘솔에서 입력받는다`() {
-        assertThat(SimulationTime.from(OnlyOneInput))
-            .isEqualTo(SimulationTime(1))
+    @ParameterizedTest
+    @ValueSource(strings = ["-1", "1.2", "x", " "])
+    fun `시간 제한의 입력이 양수가 아니라면 예외를 던진다`(rawTime: String) {
+        assertThatThrownBy { SimulationTime.of(rawTime) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(SimulationTime.INVALID_VALUE_ERROR_MESSAGE)
     }
 }
