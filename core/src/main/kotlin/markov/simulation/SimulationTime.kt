e09d9c45bd797fb7043c8e2bd30bdec3ebd37628
aksworns22 <wkdeogks17@gmail.com>
2025-11-24 00:53:16 +0900

    feat(simulation time): create simulation time from string

--- core/src/main/kotlin/markov/simulation/SimulationTime.kt
+++ core/src/main/kotlin/markov/simulation/SimulationTime.kt
@@ -1,8 +1,10 @@
 package markov.simulation
 
-import markov.input.InteractiveInput
-
 data class SimulationTime(val time: Int) {
+    init {
+        require(time >= 0) { INVALID_VALUE_ERROR_MESSAGE }
+    }
+
     override fun toString(): String {
         return time.toString()
     }
@@ -10,8 +12,10 @@ data class SimulationTime(val time: Int) {
     fun next(): SimulationTime = SimulationTime(time + 1)
 
     companion object {
-        fun from(input: InteractiveInput): SimulationTime {
-            return SimulationTime(input.readIntegerNumber())
+        const val INVALID_VALUE_ERROR_MESSAGE = "시간이 양수가 아닙니다"
+        fun of(input: String): SimulationTime {
+            val number = input.toIntOrNull() ?: throw IllegalArgumentException(INVALID_VALUE_ERROR_MESSAGE)
+            return SimulationTime(number)
         }
     }
 }
