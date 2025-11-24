4250dec2e760656509486555e603f49602a29bad
aksworns22 <wkdeogks17@gmail.com>
2025-11-24 15:47:01 +0900

    test(action.of): add additional test cases

--- core/src/test/kotlin/markov/movement/ActionTest.kt
+++ core/src/test/kotlin/markov/movement/ActionTest.kt
@@ -14,10 +14,10 @@ class ActionTest {
             .hasMessage(Action.INVALID_PROBABILITY_ERROR_MESSAGE)
     }
 
-    @Test
-    fun `이동 확률이 유효한 범위가 아니라면 예외를 발생시킨다`() {
-        assertThatThrownBy { Action.of("10,20,-10,80") }
+    @ParameterizedTest
+    @ValueSource(strings = ["10,20,-10,80", "x,100,0,0", "25.0,25,50,50", "25.5,24.5,50,50"])
+    fun `이동 확률이 유효한 범위가 아니라면 예외를 발생시킨다`(rawAction: String) {
+        assertThatThrownBy { Action.of(rawAction) }
             .isInstanceOf(IllegalArgumentException::class.java)
-            .hasMessage(Action.NEGATIVE_PROBABILITY_ERROR_MESSAGE)
     }
 }
