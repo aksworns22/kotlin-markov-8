18b450f4f029e0ea65aa61ec493f484383bfa633
aksworns22 <wkdeogks17@gmail.com>
2025-11-24 13:36:54 +0900

    refactor(data loader): remove hardcoded values

--- core/src/main/kotlin/markov/input/DataLoader.kt
+++ core/src/main/kotlin/markov/input/DataLoader.kt
@@ -5,9 +5,10 @@ import java.nio.file.Files
 import java.nio.file.Path
 
 object DataLoader {
+    const val FILE_NOT_FOUND_ERROR_MESSAGE = "파일을 찾을 수 없습니다"
     fun load(data: Data): InputStream {
         return this::class.java.classLoader.getResourceAsStream(data.path)
-            ?: throw IllegalStateException("File not found")
+            ?: throw IllegalStateException(FILE_NOT_FOUND_ERROR_MESSAGE)
     }
 
     fun load(path: Path): InputStream {
