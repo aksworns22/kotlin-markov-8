36bdfc9c7c3a43387e12e3ffb81577a3cc5a8459
aksworns22 <wkdeogks17@gmail.com>
2025-11-24 13:35:35 +0900

    refactor(simulationMapController): remove hardcoded values

--- core/src/main/kotlin/markov/map/SimulationMapController.kt
+++ core/src/main/kotlin/markov/map/SimulationMapController.kt
@@ -3,12 +3,12 @@ package markov.map
 import markov.output.Message
 import markov.output.MessageOutput
 import markov.output.MessageType
-import java.io.InputStream
 
 class SimulationMapController(val output: MessageOutput) {
     fun readMap(rawMap: List<String>): SimulationMap? {
         try {
-            val simulationMap = getSimulationMap(rawMap[0], rawMap.subList(1, rawMap.size))
+            val simulationMap =
+                getSimulationMap(rawMap[MAP_SIZE_FILE_LINE], rawMap.subList(MAP_SIZE_FILE_LINE, rawMap.size))
             output.println(Message(MessageType.SUCCESS, Message.MAP_CREATION_SUCCESS))
             return simulationMap
         } catch (err: IllegalArgumentException) {
@@ -18,13 +18,21 @@ class SimulationMapController(val output: MessageOutput) {
     }
 
     private fun getMapSize(rawMapDeclaration: String): MapSize {
-        val mapDeclaration = rawMapDeclaration.split("x")
-        require(mapDeclaration.size == 2) { MapSize.INVALID_FORMAT_ERROR_MESSAGE }
-        return MapSize.of(mapDeclaration[0], mapDeclaration[1])
+        val mapDeclaration = rawMapDeclaration.split(SIZE_DELIMITER)
+        require(mapDeclaration.size == REQUIRED_COMPONENT_SIZE) { MapSize.INVALID_FORMAT_ERROR_MESSAGE }
+        return MapSize.of(mapDeclaration[WIDTH_INDEX], mapDeclaration[HEIGHT_INDEX])
     }
 
     private fun getSimulationMap(mapDeclaration: String, rawMap: List<String>): SimulationMap {
         val mapSize = getMapSize(mapDeclaration)
         return SimulationMap.of(mapSize, rawMap)
+    }
+
+    companion object {
+        const val MAP_SIZE_FILE_LINE = 1
+        const val SIZE_DELIMITER = "x"
+        const val REQUIRED_COMPONENT_SIZE = 2
+        const val WIDTH_INDEX = 0
+        const val HEIGHT_INDEX = 1
     }
 }
