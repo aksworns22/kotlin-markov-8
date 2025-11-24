6a30176c1ff16d586b6d861ee87170f8e0856ea9
aksworns22 <wkdeogks17@gmail.com>
2025-11-24 13:18:27 +0900

    refactor(position): remove hardcoded values

--- core/src/main/kotlin/markov/map/Position.kt
+++ core/src/main/kotlin/markov/map/Position.kt
@@ -13,10 +13,20 @@ data class Position(val x: Int, val y: Int) {
     }
 
     companion object {
+        const val INVALID_FORMAT_ERROR_MESSAGE = "올바르지 못한 형식입니다"
+        const val INVALID_VALUE_ERROR_MESSAGE = "올바르지 못한 값입니다"
+        const val REQUIRED_COORDINATE_COUNT = 2
+        const val COORDINATE_DELIMITER = ","
+        const val X_COORDINATE = 0
+        const val Y_COORDINATE = 1
         fun of(rawPosition: String): Position {
-            val position = rawPosition.split(",")
-            require(position.size == 2) { "올바르지 못한 형식입니다" }
-            return Position(position[0].toInt(), position[1].toInt())
+            val coordinate = rawPosition.split(COORDINATE_DELIMITER)
+            require(coordinate.size == REQUIRED_COORDINATE_COUNT) { INVALID_FORMAT_ERROR_MESSAGE }
+            val xCoordinate =
+                coordinate[X_COORDINATE].toIntOrNull() ?: throw IllegalArgumentException(INVALID_VALUE_ERROR_MESSAGE)
+            val yCoordinate =
+                coordinate[Y_COORDINATE].toIntOrNull() ?: throw IllegalArgumentException(INVALID_VALUE_ERROR_MESSAGE)
+            return Position(xCoordinate, yCoordinate)
         }
     }
 }
