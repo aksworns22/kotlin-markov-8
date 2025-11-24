e197ed514c4d19f8eefeba040c18c2768302b875
aksworns22 <wkdeogks17@gmail.com>
2025-11-24 13:13:17 +0900

    refactor(mapReader): remove hardcoded values

--- core/src/main/kotlin/markov/map/MapReader.kt
+++ core/src/main/kotlin/markov/map/MapReader.kt
@@ -3,9 +3,11 @@ package markov.map
 import java.io.InputStream
 
 object MapReader {
+    const val INVALID_FORMAT_ERROR_MESSAGE = "유효하지 않은 형식입니다"
+    const val MIN_FILE_LINE_NUMBER = 2
     fun read(inputStream: InputStream): List<String> {
         val rawMap = inputStream.bufferedReader().readLines()
-        require(rawMap.size >= 2) { "유효하지 않은 형식입니다" }
+        require(rawMap.size >= MIN_FILE_LINE_NUMBER) { INVALID_FORMAT_ERROR_MESSAGE }
         return rawMap
     }
 }
