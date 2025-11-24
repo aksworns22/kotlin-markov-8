42e6caef3a0ae2190fbd234b759663ac98ecf576
aksworns22 <wkdeogks17@gmail.com>
2025-11-24 23:28:04 +0900

    refactor(costMapController): rename variables

--- core/src/main/kotlin/markov/cost/CostMapController.kt
+++ core/src/main/kotlin/markov/cost/CostMapController.kt
@@ -7,27 +7,27 @@ import markov.output.CostMapOutput
 class CostMapController(val output: CostMapOutput) {
     fun findCostMap(simulationMap: SimulationMap, movement: Movement): CostMap {
         val distanceMap = DistanceMap.from(simulationMap)
-        var costMap1 = CostMap.from(simulationMap.destination, movement)
-        var manual2 = costMap1.improve(simulationMap, distanceMap, movement)
-        var manual3 = manual2.improve(simulationMap, distanceMap, movement)
-        var beforeGap = costMap1.maxGapWith(manual2)
-        var afterGap = manual2.maxGapWith(manual3)
-        val epsilon = 1e-9
-        require(afterGap <= CostMap.DEFAULT_DISCOUNT_FACTOR * beforeGap + epsilon) { FINDING_MANUAL_ERROR_MESSAGE }
-        while (afterGap > STOP_THRESHOLD) {
-            costMap1 = manual2
-            manual2 = manual3
-            manual3 = manual3.improve(simulationMap, distanceMap, movement)
-            beforeGap = costMap1.maxGapWith(manual2)
-            afterGap = manual2.maxGapWith(manual3)
-            require(afterGap <= CostMap.DEFAULT_DISCOUNT_FACTOR * beforeGap + epsilon) { FINDING_MANUAL_ERROR_MESSAGE }
+        var previousCostMap = CostMap.from(simulationMap.destination, movement)
+        var currentCostMap = previousCostMap.improve(simulationMap, distanceMap, movement)
+        var nextCostMap = currentCostMap.improve(simulationMap, distanceMap, movement)
+        var previousGap = previousCostMap.maxGapWith(currentCostMap)
+        var currentGap = currentCostMap.maxGapWith(nextCostMap)
+        require(currentGap <= CostMap.DEFAULT_DISCOUNT_FACTOR * previousGap + EPSILON) { FINDING_MANUAL_ERROR_MESSAGE }
+        while (currentGap > STOP_THRESHOLD) {
+            previousCostMap = currentCostMap
+            currentCostMap = nextCostMap
+            nextCostMap = nextCostMap.improve(simulationMap, distanceMap, movement)
+            previousGap = previousCostMap.maxGapWith(currentCostMap)
+            currentGap = currentCostMap.maxGapWith(nextCostMap)
+            require(currentGap <= CostMap.DEFAULT_DISCOUNT_FACTOR * previousGap + EPSILON) { FINDING_MANUAL_ERROR_MESSAGE }
         }
-        require(afterGap <= CostMap.DEFAULT_DISCOUNT_FACTOR * beforeGap + epsilon) { FINDING_MANUAL_ERROR_MESSAGE }
-        output.println(manual3)
-        return manual3
+        require(currentGap <= CostMap.DEFAULT_DISCOUNT_FACTOR * previousGap + EPSILON) { FINDING_MANUAL_ERROR_MESSAGE }
+        output.println(nextCostMap)
+        return nextCostMap
     }
 
     companion object {
+        const val EPSILON = 1e-9
         const val STOP_THRESHOLD = 1e-3
         const val FINDING_MANUAL_ERROR_MESSAGE = "최선의 메뉴얼 탐색에 실패했습니다"
     }
